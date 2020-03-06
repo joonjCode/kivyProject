bd4885ddf8deb28a5aa8f23a63636ec50fb38bc0
joon <jtj308@naver.com>
2020-03-07 01:22:28 +0900

    Simple login kivy project Done

--- main.py
+++ main.py
@@ -1,28 +1,113 @@
-import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from database import DataBase
 
 
-class Touch(Widget):
-    btn = ObjectProperty(None)
+class CreateAccountWindow(Screen):
+    namee = ObjectProperty(None)
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
 
-    def on_touch_down(self, touch):
-        print('Mouse Down', touch)
-        self.btn.opacity = 0.5
+    def submit(self):
+        if self.namee.text != "" and self.email.text != "" and self.email.text.count(
+                "@") == 1 and self.email.text.count(".") > 0:
+            if self.password != "":
+                db.add_user(self.email.text, self.password.text, self.namee.text)
 
-    def on_touch_move(self, touch):
-        print('Mouse move', touch)
+                self.reset()
 
-    def on_touch_up(self, touch):
-        print('Mouse up', touch)
-        self.btn.opacity = 1
+                sm.current = "login"
+            else:
+                invalidForm()
+        else:
+            invalidForm()
+
+    def login(self):
+        self.reset()
+        sm.current = "login"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+        self.namee.text = ""
 
 
-class MyApp(App):
+class LoginWindow(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def loginBtn(self):
+        if db.validate(self.email.text, self.password.text):
+            MainWindow.current = self.email.text
+            self.reset()
+            sm.current = "main"
+        else:
+            invalidLogin()
+
+    def createBtn(self):
+        self.reset()
+        sm.current = "create"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+
+
+class MainWindow(Screen):
+    n = ObjectProperty(None)
+    created = ObjectProperty(None)
+    email = ObjectProperty(None)
+    current = ""
+
+    def logOut(self):
+        sm.current = "login"
+
+    def on_enter(self, *args):
+        password, name, created = db.get_user(self.current)
+        self.n.text = "Account Name: " + name
+        self.email.text = "Email: " + self.current
+        self.created.text = "Created On: " + created
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+def invalidLogin():
+    pop = Popup(title='Invalid Login',
+                content=Label(text='Invalid username or password.'),
+                size_hint=(None, None), size=(400, 400))
+    pop.open()
+
+
+def invalidForm():
+    pop = Popup(title='Invalid Form',
+                content=Label(text='Please fill in all inputs with valid information.'),
+                size_hint=(None, None), size=(400, 400))
+
+    pop.open()
+
+
+kv = Builder.load_file("my.kv")
+
+sm = WindowManager()
+db = DataBase("users.txt")
+
+screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"), MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "login"
+
+
+class MyMainApp(App):
     def build(self):
-        return Touch()
+        return sm
 
 
-if __name__ == '__main__':
-    MyApp().run()
+if __name__ == "__main__":
+    MyMainApp().run()
